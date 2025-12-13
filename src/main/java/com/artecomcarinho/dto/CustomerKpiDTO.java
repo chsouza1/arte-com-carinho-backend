e478a52da7878d51a3cf66c663bfd965dfd0c787
carlos <carloshenrique.souza88@hotmail.com>
2025-12-13 11:56:03 -0300

    ajustando bug

--- src/main/java/com/artecomcarinho/dto/CustomerKpiDTO.java
+++ src/main/java/com/artecomcarinho/dto/CustomerKpiDTO.java
@@ -6,6 +6,6 @@ import java.time.LocalDateTime;
 public record CustomerKpiDTO(
         Long customerId,
         Long ordersCount,
-        BigDecimal totalRevenue,
+        Double totalRevenue,
         LocalDateTime lastOrderDate
 ) {}
